354f829b58eeab8be870ddfa59ce4cf0aca096e4
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-11-22 23:55:34 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+    id: 236203659,
+    width: 640
+});
+
+const onPlay = function(data) {
+ };
+
+const timeUpdate = player.on('timeupdate', throttle(onPlay, 1000));
+console.log(onPlay);
+    
+localStorage.setItem("videoplayer-current-time", JSON.stringify(timeUpdate));
